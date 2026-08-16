600b7d4cb9b9c79dc88666ed0b5bd1f9efd57e2f
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-08-16 19:14:00 +0530

    fix: restore Java 25, upgrade Lombok to fix TypeTag error, and ensure Eureka registration

--- src/main/java/dev/smartpark/apigateway/ApiGatewayApplication.java
+++ src/main/java/dev/smartpark/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package dev.smartpark.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * SmartPark API Gateway
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author SmartPark Platform Team
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
